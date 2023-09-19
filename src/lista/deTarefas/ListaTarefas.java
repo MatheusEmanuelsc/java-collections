5e4aabca05fbd836f1629f70a12dd738c3ac97be
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2023-09-19 08:54:35 -0300

    Inicio coleção java

---
+++ src/lista/deTarefas/ListaTarefas.java
@@ -0,0 +1,40 @@
+package lista.deTarefas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+   private List<Tarefa> tarefas;
+
+   public ListaTarefas(){
+       this.tarefas= new ArrayList<>();
+   }
+
+   public void adicionarTarefa(String descricao){
+       tarefas.add(new Tarefa(descricao));
+   }
+
+   public void removerTarefa(String descricao){
+       List <Tarefa> removerTarefas= new ArrayList<>();
+       for(Tarefa tarefa :tarefas){
+           if (tarefa.getDescricao().equalsIgnoreCase(descricao)){
+                removerTarefas.add(tarefa);
+           }
+       }
+       tarefas.removeAll(removerTarefas);
+   }
+
+   public  int obterNumeroTotalTarefas(){
+       return  tarefas.size();
+   }
+
+   public void obterDescricoesTarefas() {
+       System.out.println(tarefas);
+   }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
